f32c7e5e2e702076af219d09b8bc9ac832ce5d54
qbz <1123757388@dianmi365.com>
2020-10-22 17:41:19 +0800

    init

---
+++ src/main/java/com/lyq/elk/HelloController.java
@@ -0,0 +1,23 @@
+package com.lyq.elk;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author qbz
+ */
+@RestController
+@Slf4j
+@RequestMapping("test/")
+public class HelloController {
+    @GetMapping("hello")
+    public String hello(){
+        log.debug("debug------------------","debugdebugdebugdebugdebugdebugdebug");
+        log.info("info----------------------","infoinfoinfoinfoinfoinfoinfoinfoinfo");
+        log.error("error-------------------","errorerrorerrorerrorerrorerrorerrorerrorerror");
+        return "hello";
+    }
+
+}
